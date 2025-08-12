09c33d8632f896665567bd4db3e986f75f5baa6f
Ashley (Scirra) <ashley@scirra.com>
2025-08-12 18:13:55 +0100

    Update to use modern JavaScript
    Make use of private properties and methods for proper encapsulation. Adjust tsconfig.json to preserve them in the output - Construct's minifier has supported all this for some time now.

--- construct-plugin/c3runtime/instance.js
+++ construct-plugin/c3runtime/instance.js
@@ -1,30 +1,32 @@
 const C3 = globalThis.C3;
 const VALID_OVERLAY_OPTIONS = ["friends", "community", "players", "settings", "official-game-group", "stats", "achievements"];
 class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
+    // Steam properties
+    #isAvailable = false;
+    #isRunningOnSteamDeck = false;
+    #accountId = 0;
+    #staticAccountKey = "";
+    #appOwnerAccountId = 0;
+    #appOwnerStaticAccountKey = "";
+    #personaName = "";
+    #playerSteamLevel = 0;
+    #appId = 0;
+    #steamUILanguage = "";
+    #currentGameLanguage = "";
+    _availableGameLanguages = ""; // note returned by an expression so is public
+    #dlcSet = new Set();
+    // For running callbacks while loading
+    #loadingTimerId = -1;
+    // For triggers. Note as these are read externally they are left as public properties
+    // but with an underscore to indicate internal use.
+    _triggerAchievement = "";
+    _triggerAppId = 0;
+    // Authentication
+    #hAuthTicket = 0;
+    #ticketHexStr = "";
     constructor() {
         // Set "scirra-steam" component ID, matching the same component ID set by the wrapper extension.
         super({ wrapperComponentId: "scirra-steam" });
-        // Steam properties
-        this._isAvailable = false;
-        this._isRunningOnSteamDeck = false;
-        this._accountId = 0;
-        this._staticAccountKey = "";
-        this._appOwnerAccountId = 0;
-        this._appOwnerStaticAccountKey = "";
-        this._personaName = "";
-        this._playerSteamLevel = 0;
-        this._appId = 0;
-        this._steamUILanguage = "";
-        this._currentGameLanguage = "";
-        this._availableGameLanguages = "";
-        this._dlcSet = new Set();
-        this._hAuthTicket = 0;
-        this._ticketHexStr = "";
-        // For running callbacks while loading
-        this._loadingTimerId = -1;
-        // For triggers
-        this._triggerAchievement = "";
-        this._triggerAppId = 0;
         // Note the properties for App ID and development mode are not read here - instead they are exported
         // to the package.json file with the call to SetWrapperExportProperties(). The wrapper extension
         // then reads these values and sends the app ID when responding to the "init" message.
@@ -32,40 +34,40 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         // Listen for overlay shown/hidden events from the extension.
         // NOTE: this is implemented but currently non-functional as the Steam overlay currently will always use
         // its fallback mode, which does not fire overlay events.
-        this._addWrapperExtensionMessageHandler("on-game-overlay-activated", e => this._onGameOverlayActivated(e));
-        this._addWrapperExtensionMessageHandler("on-dlc-installed", e => this._onDlcInstalled(e));
+        this._addWrapperExtensionMessageHandler("on-game-overlay-activated", e => this.#onGameOverlayActivated(e));
+        this._addWrapperExtensionMessageHandler("on-dlc-installed", e => this.#onDlcInstalled(e));
         // Corresponding wrapper extension is available
         if (this._isWrapperExtensionAvailable()) {
             // Run async init during loading.
-            this.runtime.sdk.addLoadPromise(this._init());
+            this.runtime.sdk.addLoadPromise(this.#init());
             // Steamworks needs the app to regularly call SteamAPI_RunCallbacks(), which is done by sending
             // the "run-callbacks" message every tick. However Construct's Tick() callback only starts
             // once the loading screen finishes. In order to allow Steam to continue running callbacks while
             // the loading screen is showing (necessary for features like the overlay), set a timer to
             // run callbacks every 20ms until the first tick, which clears the timer.
-            this._loadingTimerId = globalThis.setInterval(() => this._runCallbacks(), 20);
+            this.#loadingTimerId = globalThis.setInterval(() => this.#runCallbacks(), 20);
             this._setTicking(true);
         }
     }
-    async _init() {
+    async #init() {
         // Send init message to wrapper extension and wait for result.
         // Pass the app ID specified in properties to optionally tell Steam which app ID to use.
         const result_ = await this._sendWrapperExtensionMessageAsync("init");
         const result = result_;
         // Check availability of Steam features.
-        this._isAvailable = !!result["isAvailable"];
+        this.#isAvailable = !!result["isAvailable"];
         // If available, read the other initialization properties sent from the extension.
-        if (this._isAvailable) {
-            this._isRunningOnSteamDeck = !!result["isRunningOnSteamDeck"];
-            this._personaName = result["personaName"];
-            this._accountId = result["accountId"];
-            this._staticAccountKey = result["staticAccountKey"];
-            this._appOwnerAccountId = result["appOwnerAccountId"];
-            this._appOwnerStaticAccountKey = result["appOwnerStaticAccountKey"];
-            this._playerSteamLevel = result["playerSteamLevel"];
-            this._appId = result["appId"];
-            this._steamUILanguage = result["steamUILanguage"];
-            this._currentGameLanguage = result["currentGameLanguage"];
+        if (this.#isAvailable) {
+            this.#isRunningOnSteamDeck = !!result["isRunningOnSteamDeck"];
+            this.#personaName = result["personaName"];
+            this.#accountId = result["accountId"];
+            this.#staticAccountKey = result["staticAccountKey"];
+            this.#appOwnerAccountId = result["appOwnerAccountId"];
+            this.#appOwnerStaticAccountKey = result["appOwnerStaticAccountKey"];
+            this.#playerSteamLevel = result["playerSteamLevel"];
+            this.#appId = result["appId"];
+            this.#steamUILanguage = result["steamUILanguage"];
+            this.#currentGameLanguage = result["currentGameLanguage"];
             this._availableGameLanguages = result["availableGameLanguages"];
         }
     }
@@ -74,18 +76,18 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
     }
     _tick() {
         // On the first tick, clear the timer running callbacks for the loading screen.
-        if (this._loadingTimerId !== -1) {
-            globalThis.clearInterval(this._loadingTimerId);
-            this._loadingTimerId = -1;
+        if (this.#loadingTimerId !== -1) {
+            globalThis.clearInterval(this.#loadingTimerId);
+            this.#loadingTimerId = -1;
         }
-        this._runCallbacks();
+        this.#runCallbacks();
     }
-    _runCallbacks() {
+    #runCallbacks() {
         // Tell extension to call SteamAPI_RunCallbacks().
         this._sendWrapperExtensionMessage("run-callbacks");
     }
     // NOTE: implemented but currently non-functional
-    _onGameOverlayActivated(e) {
+    #onGameOverlayActivated(e) {
         const isShowing = e["isShowing"];
         // Dispatch scripting event and fire appropriate trigger
         // TypeScript note: cast to 'any' to add custom properties
@@ -97,18 +99,18 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         else
             this._trigger(C3.Plugins.Steamworks_Ext.Cnds.OnGameOverlayHidden);
     }
-    _onDlcInstalled(e) {
+    #onDlcInstalled(e) {
         this._triggerAppId = e["appId"];
         this._trigger(C3.Plugins.Steamworks_Ext.Cnds.OnDLCInstalled);
         this._triggerAppId = 0;
     }
     _showOverlay(option) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             return;
         this._sendWrapperExtensionMessage("show-overlay", [option]);
     }
     _showOverlayURL(url, modal) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             return;
         this._sendWrapperExtensionMessage("show-overlay-url", [url, modal]);
     }
@@ -123,37 +125,37 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
     /////////////////////////////
     // Public script interface
     get isAvailable() {
-        return this._isAvailable;
+        return this.#isAvailable;
     }
     get isRunningOnSteamDeck() {
-        return this._isRunningOnSteamDeck;
+        return this.#isRunningOnSteamDeck;
     }
     get personaName() {
-        return this._personaName;
+        return this.#personaName;
     }
     get accountId() {
-        return this._accountId;
+        return this.#accountId;
     }
     get staticAccountKey() {
-        return this._staticAccountKey;
+        return this.#staticAccountKey;
     }
     get appOwnerAccountId() {
-        return this._appOwnerAccountId;
+        return this.#appOwnerAccountId;
     }
     get appOwnerStaticAccountKey() {
-        return this._appOwnerStaticAccountKey;
+        return this.#appOwnerStaticAccountKey;
     }
     get playerSteamLevel() {
-        return this._playerSteamLevel;
+        return this.#playerSteamLevel;
     }
     get appId() {
-        return this._appId;
+        return this.#appId;
     }
     get steamUILanguage() {
-        return this._steamUILanguage;
+        return this.#steamUILanguage;
     }
     get currentGameLanguage() {
-        return this._currentGameLanguage;
+        return this.#currentGameLanguage;
     }
     // For the public script API use a method that returns an array, splitting the
     // comma-separated string
@@ -170,7 +172,7 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         this._showOverlayURL(url, modal);
     }
     async unlockAchievement(achievement) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             throw new Error("not available");
         const result_ = await this._sendWrapperExtensionMessageAsync("set-achievement", [achievement]);
         const result = result_;
@@ -188,7 +190,7 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         return isOk;
     }
     async clearAchievement(achievement) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             throw new Error("not available");
         const result_ = await this._sendWrapperExtensionMessageAsync("clear-achievement", [achievement]);
         const result = result_;
@@ -204,7 +206,7 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         return isOk;
     }
     async checkDlcInstalled(appIds) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             throw new Error("not available");
         const result_ = await this._sendWrapperExtensionMessageAsync("is-dlc-installed", [appIds.map(id => String(id)).join(",")]);
         const result = result_;
@@ -217,9 +219,9 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
                 const appId = appIds[i];
                 if (appId) {
                     if (isInstalled)
-                        this._dlcSet.add(appId);
+                        this.#dlcSet.add(appId);
                     else
-                        this._dlcSet.delete(appId);
+                        this.#dlcSet.delete(appId);
                 }
             }
         }
@@ -229,32 +231,32 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         return isOk;
     }
     isDlcInstalled(appId) {
-        return this._dlcSet.has(appId);
+        return this.#dlcSet.has(appId);
     }
     installDlc(appId) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             return;
         this._sendWrapperExtensionMessage("install-dlc", [appId]);
     }
     uninstallDlc(appId) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             return;
         this._sendWrapperExtensionMessage("uninstall-dlc", [appId]);
     }
     async getAuthTicketForWebApi(identity = "") {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             throw new Error("not available");
         const result_ = await this._sendWrapperExtensionMessageAsync("get-auth-ticket-for-web-api", [identity]);
         const result = result_;
         if (result["isOk"]) {
-            this._hAuthTicket = result["authTicket"];
-            this._ticketHexStr = result["ticketHexStr"];
+            this.#hAuthTicket = result["authTicket"];
+            this.#ticketHexStr = result["ticketHexStr"];
             // Fire success trigger
             this._trigger(C3.Plugins.Steamworks_Ext.Cnds.OnGetAuthTicketForWebApiSuccess);
             // Return obtained ticket details for script API
             return {
-                authTicket: this._hAuthTicket,
-                ticketHexStr: this._ticketHexStr
+                authTicket: this.#hAuthTicket,
+                ticketHexStr: this.#ticketHexStr
             };
         }
         else {
@@ -265,17 +267,17 @@ class Steamworks_ExtInstance extends globalThis.ISDKInstanceBase {
         }
     }
     get authTicket() {
-        return this._hAuthTicket;
+        return this.#hAuthTicket;
     }
     get ticketHexStr() {
-        return this._ticketHexStr;
+        return this.#ticketHexStr;
     }
     cancelAuthTicket(authTicket) {
-        if (!this._isAvailable)
+        if (!this.#isAvailable)
             return;
         // If the last held auth ticket is cancelled, clear it.
-        if (authTicket === this._hAuthTicket)
-            this._hAuthTicket = 0;
+        if (authTicket === this.#hAuthTicket)
+            this.#hAuthTicket = 0;
         this._sendWrapperExtensionMessage("cancel-auth-ticket", [authTicket]);
     }
 }
